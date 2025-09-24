2ed5e8da043154a6ce23a9ed16fa6f0a399ef30e
jleague317-cell <jleague317@gmail.com>
2025-09-24 10:52:50 +0530

    Simple Quiz

---
+++ Simple Quiz/app.js
@@ -0,0 +1,121 @@
+const questions = [
+    {
+        question: "Q Which planet is known as the (Red Planet)?",
+        answers: [
+            { text: "Venus", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false },
+        ]
+    },
+    {
+        question: "Q Who is known as the Father of the Nation in India?",
+        answers: [
+            { text: "Jawaharlal Nehru", correct: false },
+            { text: "Mahatma Gandhi", correct: true },
+            { text: "Subhas Chandra Bose", correct: false },
+            { text: "Sardar Patel", correct: false },
+        ]
+    },
+    {
+        question: "Q Which is the largest ocean in the world?",
+        answers: [
+            { text: "Indian Ocean", correct: false },
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true },
+            { text: "Arctic Ocean", correct: false },
+        ]
+    },
+    {
+        question: "Q Who invented the telephone?",
+        answers: [
+            { text: "Alexander Graham Bell", correct: true },
+            { text: "Thomas Edison", correct: false },
+            { text: "Nikola Tesla", correct: false },
+            { text: "James Watt", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById('question');
+const answersButton = document.getElementById('answersButton');
+const nextButton = document.getElementById('nextBtn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answersButton.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answersButton.firstChild) {
+        answersButton.removeChild(answersButton.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add('correct');
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+    Array.from(answersButton.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
